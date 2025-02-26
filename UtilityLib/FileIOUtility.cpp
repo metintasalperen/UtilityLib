c1aabd855f8ceada9cd9652ed0fee8e0defcb714
Alperen Metintaş <55763726+metintasalperen@users.noreply.github.com>
2025-02-26 21:40:19 +0300

    Seperated File IO functions to another files

---
+++ UtilityLib/FileIOUtility.cpp
@@ -0,0 +1,51 @@
+#include "FileIOUtility.h"
+
+namespace UtilityLib
+{
+    namespace FileIO
+    {
+        bool IsFileExist(const std::string& filePath)
+        {
+            std::ifstream file(filePath);
+            return file.good();
+        }
+        std::string ReadFromFile(const std::string& filePath)
+        {
+            // Open at the end to get file size
+            std::ifstream file(filePath, std::ios::ate);
+
+            // File not opened, return empty string
+            if (!file)
+            {
+                return "";
+            }
+
+            // Get file size
+            std::streamsize size = file.tellg();
+            // Move back to the beginning of file
+            file.seekg(0);
+
+            // Allocate space to string according to size
+            std::string fileContent(static_cast<size_t>(size), '\0');
+            // Read file into the string
+            file.read(&fileContent[0], size);
+            file.close();
+
+            return fileContent;
+        }
+        bool WriteToFile(const std::string& filePath, const std::string& content)
+        {
+            bool result = false;
+
+            std::ofstream file(filePath);
+            if (file.is_open())
+            {
+                file << content;
+                file.close();
+                result = true;
+            }
+
+            return result;
+        }
+    };
+};
---
+++ UtilityLib/FileIOUtility.h
@@ -0,0 +1,52 @@
+#ifndef FILEIOUTILITY_H
+#define FILEIOUTILITY_H
+
+#include <fstream>
+#include <string>
+
+namespace UtilityLib
+{
+    namespace FileIO
+    {
+        // IsFileExist()
+        // 
+        // Summary
+        // Check if specified file exist or not
+        // 
+        // Arguments:
+        // std::string "filePath"  --- In
+        // 
+        // Returns
+        // bool
+        bool IsFileExist(const std::string& filePath);
+        // ReadFile()
+        // 
+        // Summary
+        // Reads the file and returns the content as a single string
+        // 
+        // Arguments:
+        // std::string "filePath"  --- In
+        // 
+        // Returns:
+        // std::string
+        std::string ReadFromFile(const std::string& filePath);
+        // WriteFile()
+        // 
+        // Summary
+        // Writes string to the specified file
+        // 
+        // Arguments:
+        // std::string "filePath"  --- In
+        // std::string "content"   --- In
+        // 
+        // Returns:
+        // bool
+        // 
+        // Assumptions:
+        // 1. If file does not exist, a new file will be created
+        // 2. If file contains data, old data is deleted
+        bool WriteToFile(const std::string& filePath, const std::string& content);
+    };
+};
+
+#endif
