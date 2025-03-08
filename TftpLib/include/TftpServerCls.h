824a0d8985af478e1045ab0f36793b4c826fdc8b
Alperen Metintaş <55763726+metintasalperen@users.noreply.github.com>
2025-03-09 02:45:16 +0300

    Start implementing TftpServerCls with constructors and Initialize

--- TftpLib/include/TftpServerCls.h
+++ TftpLib/include/TftpServerCls.h
@@ -1,10 +1,27 @@
 #ifndef TFTPSERVERCLS_H
 #define TFTPSERVERCLS_H
 
+#include "TftpTypePkg.h"
+#include "SocketPkg.h"
+#include "FilePkg.h"
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <variant>
+#include <optional>
+#include <memory>
+
 namespace Tftp
 {
     class TftpServerCls
     {
+    private:
+        std::unique_ptr<UtilityLib::Network::SocketServerCls> Socket;
+        TftpServerCls();
+        TftpServerCls(const std::string& ipAddress);
+
+    public:
+        static std::optional<TftpServerCls> Initialize(const std::string& ipAddress);
     };
 }
 
--- TftpLib/src/TftpServerCls.cpp
+++ TftpLib/src/TftpServerCls.cpp
@@ -1,1 +1,55 @@
 #include "TftpServerCls.h"
+
+namespace Tftp
+{
+    TftpServerCls::TftpServerCls() :
+        Socket(std::make_unique<UtilityLib::Network::SocketServerCls>())
+    {
+        addrinfo hints{};
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_DGRAM;
+        hints.ai_protocol = IPPROTO_UDP;
+        Socket->SetHints(hints);
+        Socket->SetPort(TFTP_PORT);
+    }
+    TftpServerCls::TftpServerCls(const std::string& ipAddress) :
+        TftpServerCls()
+    {
+        Socket->SetIpAddress(ipAddress);
+    }
+    std::optional<TftpServerCls> TftpServerCls::Initialize(const std::string& ipAddress)
+    {
+        bool isIpAddressValid = UtilityLib::String::ValidateIpAddress(ipAddress);
+        if (!isIpAddressValid) return std::nullopt;
+
+        TftpServerCls tftpServer(ipAddress);
+
+        ErrorEnum result = tftpServer.Socket->InitializeWinsock();
+        if (result != ErrorEnum::Success)
+        {
+            return std::nullopt;
+        }
+
+        result = tftpServer.Socket->GetAddressInfo();
+        if (result != ErrorEnum::Success)
+        {
+            return std::nullopt;
+        }
+
+        result = tftpServer.Socket->CreateSocket();
+        if (result != ErrorEnum::Success)
+        {
+            return std::nullopt;
+        }
+
+        result = tftpServer.Socket->Bind();
+        if (result != ErrorEnum::Success)
+        {
+            return std::nullopt;
+        }
+
+        tftpServer.Socket->SetBlockingMode(UtilityLib::Network::BlockingMode::Blocking);
+
+        return std::make_optional<TftpServerCls>(std::move(tftpServer));
+    }
+}
